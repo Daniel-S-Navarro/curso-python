248a56f8c6f28e7e6938e8a5ce89e421979228a7
Daniel-S-Navarro <132966771+Daniel-S-Navarro@users.noreply.github.com>
2024-02-21 11:49:06 -0300

    codigo ingles

--- geradorCpf.py
+++ geradorCpf.py
@@ -3,36 +3,36 @@
 import re
 
 cpf = "746.824.890-70"
-cpf_so_numeros = re.sub(r'[^0-9]', '', cpf)
+cpf_numbers = re.sub(r'[^0-9]', '', cpf)
 
-if cpf_so_numeros[0]*len(cpf_so_numeros) == cpf_so_numeros:
+if cpf_numbers[0]*len(cpf_numbers) == cpf_numbers:
     print("CPF invalido")
 else:    
-    nove_digitos = cpf_so_numeros[:9]
-    soma = 0
-    multiplicador = 10
-    for numero in nove_digitos:
-        numero_multiplicado = int(numero) * multiplicador
-        soma += numero_multiplicado
-        multiplicador -= 1
-    primeiro_digito = (soma * 10) % 11
+    nine_digits = cpf_numbers[:9]
+    sum = 0
+    multiplier = 10
+    for number in nine_digits:
+        multiplied_number = int(number) * multiplier
+        sum += multiplied_number
+        multiplier -= 1
+    first_digit = (sum * 10) % 11
         
     # SEGUNDO DIGITO 
         
-dez_digitos = cpf_so_numeros[:10]
-print(dez_digitos)
-soma = 0
-multiplicador = 11
-for numero in dez_digitos:
-    numero_multiplicado = int(numero) * multiplicador
-    soma += numero_multiplicado
-    multiplicador -= 1
-segundo_digito = (soma * 10) % 11
+ten_digits = cpf_numbers[:10]
+print(ten_digits)
+sum = 0
+multiplier = 11
+for number in ten_digits:
+    multiplied_number = int(number) * multiplier
+    sum += multiplied_number
+    multiplier -= 1
+second_digit = (sum * 10) % 11
         
-cpf_calculado_sistema = f'{nove_digitos}{primeiro_digito}{segundo_digito}'
+cpf_calculator_system = f'{nine_digits}{first_digit}{second_digit}'
 
 
-if cpf_calculado_sistema == cpf_so_numeros:
-    print(f'O CPF: {cpf_calculado_sistema}, é válido!')
+if cpf_calculator_system == cpf_numbers:
+    print(f'O CPF: {cpf_calculator_system}, é válido!')
 else:
     print("CPF inválido")    
